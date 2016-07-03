278bf978113f2c36fe31bb50b302c8e8068b838a
Jakub Kaderka <jakub.kaderka@gmail.com>
2016-07-03 19:46:08 +0200

    Added weather functions

--- daemon/src/weather.c
+++ daemon/src/weather.c
@@ -6,6 +6,25 @@
  * Jakub Kaderka 2016
  */
 
-#include "station.h"
+#include <math.h>
+
 #include "weather.h"
 
+/*
+ * Calculate relative pressure
+ * absolute + elevation/8.3
+ */
+float press_relative(float absolute, int elevation)
+{
+	return absolute + elevation/8.3;
+}
+
+/*
+ * Calculate dew point
+ * https://en.wikipedia.org/wiki/Dew_point#Calculating_the_dew_point
+ */
+float dew_point(float humidity, float temp)
+{
+	float subres = logf(humidity/100*powf(M_E, 17.67/(243.5+temp)));
+	return 243.5*subres/(17.67-subres);
+}
---
+++ daemon/src/weather.h
@@ -0,0 +1,23 @@
+/*
+ * Weather station
+ *
+ * For license, see LICENSE.txt
+ *
+ * Jakub Kaderka 2016
+ */
+#ifndef __WEATHER_H_
+#define __WEATHER_H_
+
+/*
+ * Calculate relative pressure
+ * absolute + elevation/8.3
+ */
+extern float press_relative(float absolute, int elevation);
+
+/*
+ * Calculate dew point
+ * https://en.wikipedia.org/wiki/Dew_point#Calculating_the_dew_point
+ */
+extern float dew_point(float humidity, float temp);
+
+#endif
