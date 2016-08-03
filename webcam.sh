f9c6c25da1d60702d7be73e8c4e0f42d2db26e5b
Jakub Kaderka <jakub.kaderka@gmail.com>
2016-08-03 14:27:29 +0200

    Added removing old images from webcam

--- webcam.sh
+++ webcam.sh
@@ -7,6 +7,9 @@
 # Jakub Kaderka 2016
 #
 
+#number of files to keep stored, -1 to keep none
+KEEP=-1
+
 #Destination directory for images
 IMGDIR=/srv/www/webcam
 #Name of most recent image
@@ -15,11 +18,19 @@ LATEST=$IMGDIR/latest.jpg
 mkdir -p $IMGDIR
 
 date=$(date +"%A %d.%m.%Y %H:%M")
-filename=$(date +"%d.%m.%Y_%H.%M").jpg;
+filename=img_$(date +"%d.%m.%Y_%H.%M.%S").jpg;
 
 ffmpeg -f video4linux2 -i /dev/video0 -vframes 1 $IMGDIR/$filename &> /dev/null
 
+#add text to image
 width=$(identify -format %w $IMGDIR/$filename)
 convert -fill '#0008' -draw "rectangle 0,0,${width},20" \
 	-pointsize 20 -fill white -gravity North -annotate 0 "$date" \
 	$IMGDIR/$filename $LATEST
+
+#remove old
+DELETE=$(ls $IMGDIR/img_* | sort -r | tail -n +$(($KEEP + 1)))
+echo $DELETE
+if [[ -n $DELETE ]]; then
+	rm $DELETE
+fi
